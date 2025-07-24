25bafc442e52300edbcd6da6003965b912e7309f
Alex Mata <alexrobertomata@hotmail.com>
2025-07-24 12:14:35 -0600

    CHANGE: ruta a localhost

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,6 +1,6 @@
 const routesBackend = Object.freeze({
-  // url: 'http://localhost:3000',
-  url: 'https://todo-backend-gr-cdn.onrender.com',
+  url: 'http://localhost:3000',
+  // url: 'https://todo-backend-gr-cdn.onrender.com',
 })
 
 export const routes = Object.freeze({
